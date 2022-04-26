90044ffa9e729721939f1fb02799ee3cf0ae437d
Xavimat <xaviermatoses@gmail.com>
2022-04-26 12:59:29 +0200

    Task 2.a. Change images
    Every image has an event listener to chang its src.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
 'use strict';
 // Varita mágica, by xavimat
 
+// TASK 1. Prevent all clicks
 document.body.addEventListener('click', (e)=>e.preventDefault());
+
+// TASK 2.1. Change images on click
+// Create a randomizer function
+const randomInt = (num) => Math.floor(Math.random() * num) + 1;
+// Callback for clicks
+const imageSpell = (e) => {
+    const num = randomInt(6);
+    e.target.src = `/assets/magic-${num}.gif`;
+};
+// Get the node list of images
+const imgs = document.querySelectorAll('img');
+// Add a listener to every image
+imgs.forEach(node=>{
+    node.addEventListener('click', imageSpell)});
+
 
